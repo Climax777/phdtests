711742fbac8ce77ee04f33f4019256bb854f4353
Pieter Willem Jordaan <pieterwjordaanpc@gmail.com>
2019-03-14 23:35:03 +0200

    Struggling with RPATH on mac, workaround is DYLD_...
    Basic test and benches platform ready. Need to implement specific tests
    now

--- include/dbphd/mongodb/mongodb.hpp
+++ include/dbphd/mongodb/mongodb.hpp
@@ -1,14 +1,34 @@
 #ifndef MONGODB_HPP
 #define MONGODB_HPP
 
+#include <memory>
+
+#include <bsoncxx/builder/stream/array.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/builder/core.hpp>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/types.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/pool.hpp>
+#include <mongocxx/uri.hpp>
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/builder/basic/kvp.hpp>
+#include <bsoncxx/document/value.hpp>
+#include <bsoncxx/document/view.hpp>
+#include <bsoncxx/document/view_or_value.hpp>
+
 class MongoDBHandler
 {
 private:
-	
+	static std::shared_ptr<mongocxx::pool> m_Pool;
 
 public:
 	MongoDBHandler();
 	virtual ~MongoDBHandler();
+	
+	static mongocxx::pool::entry GetConnection(std::string connstr = "mongodb://localhost:27017/?maxPoolSize=100");
 };
 
 #endif /* MONGODB_HPP */
--- include/dbphd/mysqldb/mysqldb.hpp
+++ include/dbphd/mysqldb/mysqldb.hpp
@@ -1,6 +1,21 @@
 #ifndef MYSQLDB_HPP
 #define MYSQLDB_HPP
 
+#include <memory>
+#include <mysqlx/xdevapi.h>
+
+/*using ::std::cout;
+using ::std::endl;
+using namespace ::mysqlx;
+#undef throw
+#define throw() noexcept
+#include <mysql_connection.h>
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>*/
+
 class MySQLDBHandler
 {
 private:
@@ -9,6 +24,8 @@ private:
 public:
 	MySQLDBHandler();
 	virtual ~MySQLDBHandler();
+
+	static mysqlx::Session GetConnection(std::string connstr = "mysqlx://root:ABB03075023084a@127.0.0.1");
 };
 
 #endif /* MYSQLDB_HPP */
