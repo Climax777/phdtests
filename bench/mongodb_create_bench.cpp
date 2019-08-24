a73eae60ebd4884290b796eb4143077f747fec1d
Pieter Willem Jordaan <pieterwjordaanpc@gmail.com>
2019-08-24 23:29:55 +0200

    Insert benches done Signed-off-by: Pieter Willem Jordaan <pieterwjordaanpc@gmail.com>

--- bench/mongodb_create_bench.cpp
+++ bench/mongodb_create_bench.cpp
@@ -1,12 +1,16 @@
 #include "benchmark/benchmark.h"
 #include "dbphd/mongodb/mongodb.hpp"
+#include <random>
+#include <string>
+
+using namespace std;
 using bsoncxx::builder::basic::kvp;
 using bsoncxx::builder::basic::make_document;
 
 static void CustomArgumentsInserts(benchmark::internal::Benchmark* b) {
-	for (int i = 1; i <= (1 << 16); i*=8) {
-		for (int j = 1; j <= 16; j *= 2) {
-			for(int k = 0; k <= j; ) {
+	for (int i = 1; i <= (1 << 12); i*=8) { // Documents
+		for (int j = 1; j <= 16; j *= 2) { // Fields
+			for(int k = 0; k <= j; ) { // Indexes
 				b->Args({i, j, k});
 				if(k == 0) {
 					k = 1;
@@ -21,29 +25,95 @@ static void CustomArgumentsInserts(benchmark::internal::Benchmark* b) {
 static void BM_MONGO_Insert(benchmark::State& state) {
 	auto conn = MongoDBHandler::GetConnection();
 	auto db = conn->database("bench");
-	auto collection = db.collection("select_bench");
-	mongocxx::pipeline stages;
-	stages.limit(1);
-	stages.project(make_document(kvp("_id", 0), kvp("result", make_document(kvp("$literal", 500)))));
+	auto collection = db.collection("create_bench");
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> dis(0, (1 << 16));
 	// Per thread settings...
 	if(state.thread_index == 0) {
 		// This is the first thread, so do initialization here, build indexes etc...
-	}
-
-	for(auto _ : state) {
-		for(int x = 0; x < state.range(0); ++x) {
-			auto cursor = collection.aggregate(stages);
+		collection.drop();
+		collection.create_index(make_document(kvp("_id", 1)));
+		if(state.range(2) > 0) {
+			for(int index = 0; index < state.range(2); ++index) {
+				collection.create_index(make_document(kvp(("a" + to_string(index)), 1)));
+			}
 		}
 	}
+	for(auto _ : state) {
+		state.PauseTiming();
+		std::vector<bsoncxx::document::value> documents;
+		for(int n = 0; n < state.range(0); ++n) {
+			auto builder = bsoncxx::builder::stream::document{};
+			auto doc = builder << "a0" << dis(gen);
+			for(int fields = 1; fields < state.range(1); ++fields) {
+				doc << ("a" + to_string(fields)) << to_string(dis(gen));
+			}
+
+			documents.push_back(doc << bsoncxx::builder::stream::finalize);
+		}
+		state.ResumeTiming();
+		collection.insert_many(documents);
+	}
+
+	if(state.thread_index == 0) {
+		collection.drop();
+		// This is the first thread, so do destruction here (delete documents etc..)
+	}
+
 	state.SetItemsProcessed(state.iterations()*state.range(0));
-	state.counters.insert({{"Fields", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Indexes", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
+	state.counters.insert({{"Documents", benchmark::Counter(state.range(0), benchmark::Counter::kAvgThreads)}, {"Fields", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Indexes", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
 }
 
-BENCHMARK(BM_MONGO_Insert)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity();
-BENCHMARK(BM_MONGO_Insert)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->Threads(2);
-BENCHMARK(BM_MONGO_Insert)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->Threads(3);
-BENCHMARK(BM_MONGO_Insert)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->Threads(4);
+BENCHMARK(BM_MONGO_Insert)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->ThreadRange(1,4);
 
+static void BM_MONGO_InsertTransact(benchmark::State& state) {
+	auto conn = MongoDBHandler::GetConnection();
+	auto db = conn->database("bench");
+	auto collection = db.collection("create_bench");
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> dis(0, (1 << 16));
+	// Per thread settings...
+	if(state.thread_index == 0) {
+		// This is the first thread, so do initialization here, build indexes etc...
+		collection.drop();
+		collection.create_index(make_document(kvp("_id", 1)));
+		if(state.range(2) > 0) {
+			for(int index = 0; index < state.range(2); ++index) {
+				collection.create_index(make_document(kvp(("a" + to_string(index)), 1)));
+			}
+		}
+	}
+	auto session = conn->start_session();
+	for(auto _ : state) {
+		state.PauseTiming();
+		std::vector<bsoncxx::document::value> documents;
+		for(int n = 0; n < state.range(0); ++n) {
+			auto builder = bsoncxx::builder::stream::document{};
+			auto doc = builder << "a0" << dis(gen);
+			for(int fields = 1; fields < state.range(1); ++fields) {
+				doc << ("a" + to_string(fields)) << to_string(dis(gen));
+			}
+
+			documents.push_back(doc << bsoncxx::builder::stream::finalize);
+		}
+		state.ResumeTiming();
+		session.start_transaction();
+		collection.insert_many(documents);
+		session.commit_transaction();
+	}
+
+	if(state.thread_index == 0) {
+		collection.drop();
+		// This is the first thread, so do destruction here (delete documents etc..)
+	}
+
+	state.SetItemsProcessed(state.iterations()*state.range(0));
+	state.counters.insert({{"Documents", benchmark::Counter(state.range(0), benchmark::Counter::kAvgThreads)}, {"Fields", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Indexes", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
+}
+
+BENCHMARK(BM_MONGO_InsertTransact)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->ThreadRange(1,4);
 /*static void BM_MONGO_SelectTransact(benchmark::State& state) {
 	auto conn = MongoDBHandler::GetConnection();
 	auto db = conn->database("bench");
--- bench/mysql_create_bench.cpp
+++ bench/mysql_create_bench.cpp
@@ -1,17 +1,142 @@
 #include "benchmark/benchmark.h"
 #include "dbphd/mysqldb/mysqldb.hpp"
 
-/*static void BM_MYSQL_Select(benchmark::State& state) {
-	auto conn = MySQLDBHandler::GetConnection();	
-	const std::string query = "select 500";
-	for(auto _ : state) {
-		auto querystatement = conn.sql(query);
-		auto result = querystatement.execute();
+#include <random>
+
+using namespace std;
+
+static void CustomArgumentsInserts(benchmark::internal::Benchmark* b) {
+	for (int i = 1; i <= (1 << 12); i*=8) { // Documents
+		for (int j = 1; j <= 16; j *= 2) { // Fields
+			for(int k = 0; k <= j; ) { // Indexes
+				b->Args({i, j, k});
+				if(k == 0) {
+					k = 1;
+				} else {
+					k *= 2;
+				}
+			}
+		}
 	}
 }
 
-BENCHMARK(BM_MYSQL_Select);
+static void BM_MYSQL_Insert(benchmark::State& state) {
+	auto conn = MySQLDBHandler::GetConnection();	
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> dis(0, (1 << 16));
+	// Per thread settings...
+	if(state.thread_index == 0) {
+		// This is the first thread, so do initialization here, build indexes etc...
+		MySQLDBHandler::CreateDatabase(conn, "bench");
+		MySQLDBHandler::DropTable(conn, "bench", "create_bench");
+		string createQuery = R"|(
+CREATE TABLE create_bench (
+	_id  INT AUTO_INCREMENT,
+	a0 INT,
+)|";
+		for(int field = 1; field < state.range(1); ++field) {
+			createQuery.append("a" + to_string(field) + " INT,");
+		}
+		createQuery.append(R"|(
+	PRIMARY KEY(_id)
+) ENGINE=INNODB;
+)|");
+		conn.sql(createQuery).execute();
+		if(state.range(2) > 0) {
+			for(int index = 0; index < state.range(2); ++index) {
+				conn.sql("CREATE INDEX field" + to_string(index) + " ON bench.create_bench(a" + to_string(index) + ");").execute();
+			}
+		}
+	}
+	auto db = conn.getSchema("bench");
+	auto table = db.getTable("create_bench");
+	for(auto _ : state) {
+		state.PauseTiming();
+		auto tableInsert = table.insert();
+		for(int n = 0; n < state.range(0); ++n) {
+			mysqlx::Row row;
+				row.set(0, mysqlx::nullvalue);
+			for(int fields = 0; fields < state.range(1); ++fields) {
+				row.set(fields+1, dis(gen));
+			}
+			tableInsert.rows(row);
+		}
+		state.ResumeTiming();
+		auto result = tableInsert.execute();
+	}
 
+	if(state.thread_index == 0) {
+		MySQLDBHandler::DropTable(conn, "bench", "create_bench");
+		// This is the first thread, so do destruction here (delete documents etc..)
+	}
+
+	state.SetItemsProcessed(state.iterations()*state.range(0));
+	state.counters.insert({{"Documents", benchmark::Counter(state.range(0), benchmark::Counter::kAvgThreads)}, {"Fields", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Indexes", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
+}
+
+BENCHMARK(BM_MYSQL_Insert)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->ThreadRange(1,4);
+
+static void BM_MYSQL_InsertTransact(benchmark::State& state) {
+	auto conn = MySQLDBHandler::GetConnection();	
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> dis(0, (1 << 16));
+	const std::string query = "select 500";
+	// Per thread settings...
+	if(state.thread_index == 0) {
+		// This is the first thread, so do initialization here, build indexes etc...
+		MySQLDBHandler::CreateDatabase(conn, "bench");
+		MySQLDBHandler::DropTable(conn, "bench", "create_bench");
+		string createQuery = R"|(
+CREATE TABLE create_bench (
+	_id  INT AUTO_INCREMENT,
+	a0 INT,
+)|";
+		for(int field = 1; field < state.range(1); ++field) {
+			createQuery.append("a" + to_string(field) + " INT,");
+		}
+		createQuery.append(R"|(
+	PRIMARY KEY(_id)
+) ENGINE=INNODB;
+)|");
+		conn.sql(createQuery).execute();
+		if(state.range(2) > 0) {
+			for(int index = 0; index < state.range(2); ++index) {
+				conn.sql("CREATE INDEX field" + to_string(index) + " ON bench.create_bench(a" + to_string(index) + ");").execute();
+			}
+		}
+	}
+	auto db = conn.getSchema("bench");
+	auto table = db.getTable("create_bench");
+	for(auto _ : state) {
+		state.PauseTiming();
+		auto tableInsert = table.insert();
+		for(int n = 0; n < state.range(0); ++n) {
+			mysqlx::Row row;
+				row.set(0, mysqlx::nullvalue);
+			for(int fields = 0; fields < state.range(1); ++fields) {
+				row.set(fields+1, dis(gen));
+			}
+			tableInsert.rows(row);
+		}
+		state.ResumeTiming();
+		conn.startTransaction();
+		auto result = tableInsert.execute();
+		conn.commit();
+	}
+
+	if(state.thread_index == 0) {
+		MySQLDBHandler::DropTable(conn, "bench", "create_bench");
+		// This is the first thread, so do destruction here (delete documents etc..)
+	}
+
+	state.SetItemsProcessed(state.iterations()*state.range(0));
+	state.counters.insert({{"Documents", benchmark::Counter(state.range(0), benchmark::Counter::kAvgThreads)}, {"Fields", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Indexes", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
+}
+
+BENCHMARK(BM_MYSQL_InsertTransact)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->ThreadRange(1,4);
+/*
 static void BM_MYSQL_SelectTransact(benchmark::State& state) {
 	auto conn = MySQLDBHandler::GetConnection();	
 	const std::string query = "select 500";
