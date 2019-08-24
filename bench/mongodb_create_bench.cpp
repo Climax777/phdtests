ba91a4cc7bb100efe459230c2dd74a95567c5f64
Pieter Willem Jordaan <pieterwjordaanpc@gmail.com>
2019-08-24 23:43:29 +0200

    Removed UseRealtime Signed-off-by: Pieter Willem Jordaan <pieterwjordaanpc@gmail.com>

--- bench/mongodb_create_bench.cpp
+++ bench/mongodb_create_bench.cpp
@@ -74,7 +74,7 @@ static void BM_MONGO_Insert(benchmark::State& state) {
 	state.counters.insert({{"Documents", benchmark::Counter(state.range(0), benchmark::Counter::kAvgThreads)}, {"Fields", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Indexes", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
 }
 
-BENCHMARK(BM_MONGO_Insert)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->ThreadRange(1,4);
+BENCHMARK(BM_MONGO_Insert)->Apply(CustomArgumentsInserts)->Complexity()->DenseThreadRange(1,4);
 
 static void BM_MONGO_InsertTransact(benchmark::State& state) {
 	auto conn = MongoDBHandler::GetConnection();
@@ -131,7 +131,7 @@ static void BM_MONGO_InsertTransact(benchmark::State& state) {
 	state.counters.insert({{"Documents", benchmark::Counter(state.range(0), benchmark::Counter::kAvgThreads)}, {"Fields", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Indexes", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
 }
 
-BENCHMARK(BM_MONGO_InsertTransact)->Apply(CustomArgumentsInserts)->UseRealTime()->Complexity()->ThreadRange(1,4);
+BENCHMARK(BM_MONGO_InsertTransact)->Apply(CustomArgumentsInserts)->Complexity()->DenseThreadRange(1,4);
 /*static void BM_MONGO_SelectTransact(benchmark::State& state) {
 	auto conn = MongoDBHandler::GetConnection();
 	auto db = conn->database("bench");
