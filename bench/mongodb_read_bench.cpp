7a9c8882146d4144e08de21a3c13464d4d3d9d57
Pieter Willem Jordaan <pieterwjordaanpc@gmail.com>
2019-10-08 23:33:06 +0200

    Document update tests TBD. Add mongo manual join

--- bench/mongodb_read_bench.cpp
+++ bench/mongodb_read_bench.cpp
@@ -16,6 +16,7 @@
 // *	Simple aggregations: avg, sum, *2 Done
 // *	sort // Done
 // *	Join Done
+// *	Manual Join Done
 // *	Skip Not really more impactful than just returning the document
 // *	Range queries // This is implicitly the same as fixing one and leaving the rest of the columns >= 0 ???
 
@@ -63,6 +64,16 @@ static void CustomArgumentsInserts5(benchmark::internal::Benchmark* b) {
 	for (int i = 0; i <= 2; ++i) { // fields to index
 		for(int l = 1; l <= (int)pow(Precalculator::Values,Precalculator::Columns); l *= 2) { // Documents to return
 			b->Args({i, l});
+		}
+	}
+}
+
+static void CustomArgumentsInserts6(benchmark::internal::Benchmark* b) {
+	for (int i = 0; i <= 2; ++i) { // fields to index
+		for(int j = 1; j <= (int)pow(Precalculator::Values,Precalculator::Columns); j *= 2) { // Documents to return
+			for(int k = 1; k <= j; k *= 2) { // Documents to query for batch
+				b->Args({i, j, k});
+			}
 		}
 	}
 }
@@ -970,3 +981,181 @@ static void BM_MONGO_Read_Join_Transact(benchmark::State& state) {
 }
 
 BENCHMARK(BM_MONGO_Read_Join_Transact)->Apply(CustomArgumentsInserts5)->Complexity()->DenseThreadRange(1,4);
+
+static void BM_MONGO_Read_Join_Manual(benchmark::State& state) {
+	auto conn = MongoDBHandler::GetConnection();
+	auto db = conn->database("bench");
+	auto collection = db.collection("read_bench");
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> dis(0, 4);
+	// Per thread settings...
+	if(state.thread_index == 0) {
+		// This is the first thread, so do initialization here, build indexes etc...
+		CreateCollection(conn);
+		collection = db.collection("read_bench");
+		collection.indexes().drop_all();
+		mongocxx::options::index index_options{};
+		index_options.background(false);
+		for(int index = 0; index < state.range(0); ++index) {
+			auto idxbuilder = bsoncxx::builder::stream::document{};
+			idxbuilder << ("a" + to_string(index)) << 1;
+			// One index (basically just many indexes)
+			collection.create_index(idxbuilder << bsoncxx::builder::stream::finalize, index_options);
+		}
+	}
+	auto session = conn->start_session();
+	uint64_t count = 0;
+	for(auto _ : state) {
+		state.PauseTiming();
+		vector<bsoncxx::document::view> batch(state.range(2));
+		vector<pair<bsoncxx::document::view, vector<bsoncxx::document::view>>> results(state.range(1));
+		state.ResumeTiming();
+		mongocxx::options::find opts;
+		opts.limit(state.range(1));
+		auto cursor = collection.find(session, {}, opts);
+		for(auto doc : cursor) {
+			batch.push_back(doc);
+			// Our batch is ready...
+			if(batch.size() == state.range(2)) {
+				auto query = bsoncxx::builder::stream::document{};
+				auto partial = query << "$or" << bsoncxx::builder::stream::open_array;
+				for(auto querydoc : batch) {
+					vector<bsoncxx::document::view> a;
+					results.push_back(make_pair(querydoc, a));
+					partial = partial << bsoncxx::builder::stream::open_document << "$and" << bsoncxx::builder::stream::open_array << bsoncxx::builder::stream::open_document << "a1" << querydoc["a0"].get_int64() << bsoncxx::builder::stream::close_document << bsoncxx::builder::stream::open_document << "_id"  << bsoncxx::builder::stream::open_document << "$ne" << querydoc["_id"].get_oid() << bsoncxx::builder::stream::close_document << bsoncxx::builder::stream::close_document << bsoncxx::builder::stream::close_array << bsoncxx::builder::stream::close_document;
+				} 
+				mongocxx::options::find opts;
+				opts.limit(state.range(1));
+				auto afterin = partial << bsoncxx::builder::stream::close_array;
+				auto cursorinternal = collection.find(session, afterin << bsoncxx::builder::stream::finalize, opts);
+				for(auto doc : cursor) {
+					for(auto res: results) {
+						if(res.first["a0"].get_int64() == doc["a0"].get_int64()) {
+							res.second.push_back(doc);
+							++count;
+							break;
+						}
+					}
+					if(count >= state.range(1))
+						break;
+				}
+				batch.clear();
+			}
+			if(count >= state.range(1))
+				break;
+		}
+	}
+
+	if(state.thread_index == 0) {
+		//collection.drop();
+		// This is the first thread, so do destruction here (delete documents etc..)
+		// TODO Figure out way to kill collection after all tests of suite is done
+	}
+	state.SetComplexityN(state.range(1));
+
+	state.SetItemsProcessed(state.range(1)*state.iterations());
+
+	// Set the counter as a rate. It will be presented divided
+	// by the duration of the benchmark.
+	// Meaning: per one second, how many 'foo's are processed?
+	state.counters["Ops"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+
+	// Set the counter as a rate. It will be presented divided
+	// by the duration of the benchmark, and the result inverted.
+	// Meaning: how many seconds it takes to process one 'foo'?
+	state.counters["OpsInv"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+	state.counters.insert({{"Indexes", benchmark::Counter(state.range(0), benchmark::Counter::kAvgThreads)}, {"Limit", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Batch", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
+}
+
+BENCHMARK(BM_MONGO_Read_Join_Manual)->Apply(CustomArgumentsInserts6)->Complexity()->DenseThreadRange(1,4);
+ 
+static void BM_MONGO_Read_Join_Manual_Transact(benchmark::State& state) {
+	auto conn = MongoDBHandler::GetConnection();
+	auto db = conn->database("bench");
+	auto collection = db.collection("read_bench");
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> dis(0, 4);
+	// Per thread settings...
+	if(state.thread_index == 0) {
+		// This is the first thread, so do initialization here, build indexes etc...
+		CreateCollection(conn);
+		collection = db.collection("read_bench");
+		collection.indexes().drop_all();
+		mongocxx::options::index index_options{};
+		index_options.background(false);
+		for(int index = 0; index < state.range(0); ++index) {
+			auto idxbuilder = bsoncxx::builder::stream::document{};
+			idxbuilder << ("a" + to_string(index)) << 1;
+			// One index (basically just many indexes)
+			collection.create_index(idxbuilder << bsoncxx::builder::stream::finalize, index_options);
+		}
+	}
+	auto session = conn->start_session();
+	uint64_t count = 0;
+	for(auto _ : state) {
+		state.PauseTiming();
+		vector<bsoncxx::document::view> batch(state.range(2));
+		vector<pair<bsoncxx::document::view, vector<bsoncxx::document::view>>> results(state.range(1));
+		state.ResumeTiming();
+		session.start_transaction();
+		mongocxx::options::find opts;
+		opts.limit(state.range(1));
+		auto cursor = collection.find(session, {}, opts);
+		for(auto doc : cursor) {
+			batch.push_back(doc);
+			// Our batch is ready...
+			if(batch.size() == state.range(2)) {
+				auto query = bsoncxx::builder::stream::document{};
+				auto partial = query << "$or" << bsoncxx::builder::stream::open_array;
+				for(auto querydoc : batch) {
+					vector<bsoncxx::document::view> a;
+					results.push_back(make_pair(querydoc, a));
+					partial = partial << bsoncxx::builder::stream::open_document << "$and" << bsoncxx::builder::stream::open_array << bsoncxx::builder::stream::open_document << "a1" << querydoc["a0"].get_int64() << bsoncxx::builder::stream::close_document << bsoncxx::builder::stream::open_document << "_id"  << bsoncxx::builder::stream::open_document << "$ne" << querydoc["_id"].get_oid() << bsoncxx::builder::stream::close_document << bsoncxx::builder::stream::close_document << bsoncxx::builder::stream::close_array << bsoncxx::builder::stream::close_document;
+				} 
+				mongocxx::options::find opts;
+				opts.limit(state.range(1));
+				auto afterin = partial << bsoncxx::builder::stream::close_array;
+				auto cursorinternal = collection.find(session, afterin << bsoncxx::builder::stream::finalize, opts);
+				for(auto doc : cursor) {
+					for(auto res: results) {
+						if(res.first["a0"].get_int64() == doc["a0"].get_int64()) {
+							res.second.push_back(doc);
+							++count;
+							break;
+						}
+					}
+					if(count >= state.range(1))
+						break;
+				}
+				batch.clear();
+			}
+			if(count >= state.range(1))
+				break;
+		}
+		session.commit_transaction();
+	}
+
+	if(state.thread_index == 0) {
+		//collection.drop();
+		// This is the first thread, so do destruction here (delete documents etc..)
+		// TODO Figure out way to kill collection after all tests of suite is done
+	}
+	state.SetComplexityN(state.range(1));
+
+	state.SetItemsProcessed(state.range(1)*state.iterations());
+
+	// Set the counter as a rate. It will be presented divided
+	// by the duration of the benchmark.
+	// Meaning: per one second, how many 'foo's are processed?
+	state.counters["Ops"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+
+	// Set the counter as a rate. It will be presented divided
+	// by the duration of the benchmark, and the result inverted.
+	// Meaning: how many seconds it takes to process one 'foo'?
+	state.counters["OpsInv"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+	state.counters.insert({{"Indexes", benchmark::Counter(state.range(0), benchmark::Counter::kAvgThreads)}, {"Limit", benchmark::Counter(state.range(1), benchmark::Counter::kAvgThreads)}, {"Batch", benchmark::Counter(state.range(2), benchmark::Counter::kAvgThreads)}});
+}
+
+BENCHMARK(BM_MONGO_Read_Join_Manual_Transact)->Apply(CustomArgumentsInserts6)->Complexity()->DenseThreadRange(1,4);
